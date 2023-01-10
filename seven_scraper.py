4d6a82ccb17733c3fef71cdb760eedb131f85e83
Ehsan U <Ehsan-U>
2023-01-10 14:31:08 +0500

    4 spiders added (under-development)

--- seven_scraper.py
+++ seven_scraper.py
@@ -9,8 +9,8 @@ class Seven_Scraper(Spider):
     name = 'seven_spiders'
     # iadfrance.fr
     iadfrance_info = {
-        'page':1,
-        'group_ids':[]
+        'page': 1,
+        'group_ids': []
     }
     # safti.fr
     safti_info = {
@@ -22,9 +22,17 @@ class Seven_Scraper(Spider):
             'Accept': '*/*',
             'host': 'api.safti.fr',
         },
-        'next':True,
-        'uuids':set(),
-        'limit':4000
+        'next': True,
+        'uuids': set(),
+        'limit': 4000
+    }
+    # bskimmobilier.com
+    bskimmobilier_info = {
+        'page': 1
+    }
+    # megagence
+    megagence_info = {
+        'page': 1
     }
 
 
@@ -32,6 +40,8 @@ class Seven_Scraper(Spider):
         urls = {
             "https://www.iadfrance.fr/trouver-un-conseiller/hauts-de-france":self.parse_iadfrance,
             "https://api.safti.fr/public_site/agent/search":self.parse_safti,
+            "https://bskimmobilier.com/commercial-immobilier": self.parse_bskimmobilier,
+            "https://www.megagence.com/nos-consultants": self.parse_megagence,
         }
 
         for url, parse_method in urls.items():
@@ -57,16 +67,8 @@ class Seven_Scraper(Spider):
             }
             yield item
         if response.xpath("//div[@class='show--more-text']"):
-            next_url = self.iadfrance_nextPage(response)
-            yield scrapy.Request(url=next_url, callback=self.parse_iadfrance)
-
-    def iadfrance_nextPage(self, response):
-        self.iadfrance_info['page'] += 1
-        page = self.iadfrance_info['page']
-        self.iadfrance_info['group_ids'] += response.xpath("//div[contains(@class,'agent_card') and contains(@class,'onResult')]//a[contains(@data-gtm, 'email')]/@id").getall()
-        ignore_ids = "-".join(self.iadfrance_info['group_ids'])
-        next_url = f"https://www.iadfrance.fr/trouver-un-conseiller/hauts-de-france?ignore_ids={ignore_ids}&page={page}"
-        return next_url
+            next_page = self.get_nextPage(website=response.url, response=response)
+            yield scrapy.Request(url=next_page, callback=self.parse_iadfrance)
 
 
     def parse_safti(self, response):
@@ -89,15 +91,73 @@ class Seven_Scraper(Spider):
                        }
                     yield item
         if len(self.safti_info['uuids']) <= self.safti_info['limit']:
-            payload = self.safti_nextPage()
+            payload = self.get_nextPage(website=response.url)
             yield scrapy.FormRequest(url=response.url, callback=self.parse_safti, formdata=payload, headers=self.safti_info['safti_headers'])
 
-    def safti_nextPage(self):
-        self.safti_info['page'] += 1
-        page = self.safti_info['page']
-        payload = {"page": str(page), "limit": '9'}
-        return payload
 
+    def parse_bskimmobilier(self, response):
+        for agent in response.xpath("//div[@class='informations']"):
+            name = agent.xpath(".//div[@class='name']/text()").get()
+            phone = agent.xpath(".//div[@class='phone']/a/text()").get()
+            address = agent.xpath(".//div[@class='city']/text()").get()
+            email = agent.xpath(".//div[@class='mail']/a/text()").get()
+            item = {
+                "name": self.clean(name),
+                "phone": self.clean(phone),
+                "address": self.clean(address),
+                "email": self.clean(email)
+            }
+            yield item
+        if response.xpath("//div[@class='informations']"):
+            next_page = self.get_nextPage(website=response.url)
+            yield scrapy.Request(url=next_page, callback=self.parse_bskimmobilier)
+
+
+    def parse_megagence(self, response):
+        for agent in response.xpath("//article[@class='counsellor-list']"):
+            name = agent.xpath(".//div[@class='counsellor-list-name']/text()").getall()
+            phone = agent.xpath(".//div[@class='counsellor-list-phone']/a/text()").get()
+            address = agent.xpath("//article[@class='counsellor-list']//div[@class='counsellor-list-location']/text()").get()
+            email = ''
+            item = {
+                "name": self.clean(name),
+                "phone": self.clean(phone),
+                "address": self.clean(address),
+                "email": self.clean(email)
+            }
+            yield item
+        if response.xpath("//article[@class='counsellor-list']"):
+            next_page = self.get_nextPage(website=response.url)
+            yield scrapy.Request(url=next_page, callback=self.parse_megagence)
+
+
+    def get_nextPage(self, website, response=None):
+        if "iadfrance.fr" in website:
+            self.iadfrance_info['page'] += 1
+            page = self.iadfrance_info['page']
+            self.iadfrance_info['group_ids'] += response.xpath(
+                "//div[contains(@class,'agent_card') and contains(@class,'onResult')]//a[contains(@data-gtm, 'email')]/@id").getall()
+            ignore_ids = "-".join(self.iadfrance_info['group_ids'])
+            next_page = f"https://www.iadfrance.fr/trouver-un-conseiller/hauts-de-france?ignore_ids={ignore_ids}&page={page}"
+            return next_page
+
+        elif "safti.fr" in website:
+            self.safti_info['page'] += 1
+            page = self.safti_info['page']
+            payload = {"page": str(page), "limit": '9'}
+            return payload
+
+        elif "bskimmobilier.com" in website:
+            self.bskimmobilier_info['page'] += 1
+            page = self.bskimmobilier_info['page']
+            next_page = f'https://bskimmobilier.com/commercial-immobilier?page={str(page)}'
+            return next_page
+
+        elif "megagence.com" in website:
+            self.megagence_info['page'] += 1
+            page = self.megagence_info['page']
+            next_page = f'https://www.megagence.com/nos-consultants?page={str(page)}'
+            return next_page
 
     @staticmethod
     def clean(data):
@@ -109,11 +169,10 @@ class Seven_Scraper(Spider):
             return ''
 
 
-
 crawler = CrawlerProcess(settings={
     "REQUEST_FINGERPRINTER_IMPLEMENTATION": '2.7',
     "ROBOTSTXT_OBEY": False,
-    "LOG_LEVEL":logging.INFO,
+    "LOG_LEVEL":logging.DEBUG,
     "USER_AGENT": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
     "HTTPCACHE_ENABLED": True,
     "FEEDS":{"data.csv":{'format':'csv'}}
@@ -122,4 +181,4 @@ crawler = CrawlerProcess(settings={
 crawler.crawl(Seven_Scraper)
 crawler.start()
 
-# "https://bskimmobilier.com/commercial-immobilier":self.parse_bskimmobilier
+#
